147b49868af902f751f70e94d7897c7228328a7b
unknown <lawlerg@LAPTOP1035.V1.com>
2015-07-21 10:50:46 +0100

    add annotated queue listener

--- listener/src/java/starter/app/listener/JmsAnnotationListener.java
+++ listener/src/java/starter/app/listener/JmsAnnotationListener.java
@@ -12,11 +12,22 @@ import javax.jms.Message;
 public class JmsAnnotationListener {
 
     @JmsListener(destination = "topic.test", subscription = "annotatedDurableSub")
-    public void listen(Message message) {
+    public void listenToTopic(Message message) {
         try {
             CustomMessage customMessage = new CustomMessage(message.getStringProperty(JmsPropertyConstants.THE_MESSAGE));
             Thread.sleep(1000);
             System.out.println("Annotated listener: message received: " + customMessage.getMyMessage());
+        } catch (JMSException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @JmsListener(destination = "queue.test", containerFactory = "jmsQueueListenerContainerFactory")
+    public void listenToQueue(Message message) {
+        try {
+            CustomMessage customMessage = new CustomMessage(message.getStringProperty(JmsPropertyConstants.THE_MESSAGE));
+            Thread.sleep(1000);
+            System.out.println("Annotated queue listener: message received: " + customMessage.getMyMessage());
         } catch (JMSException | InterruptedException e) {
             throw new RuntimeException(e);
         }
--- listener/src/java/starter/config/AnnotationConfig.java
+++ listener/src/java/starter/config/AnnotationConfig.java
@@ -1,10 +1,13 @@
 package starter.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.support.destination.DestinationResolver;
 import org.springframework.jms.support.destination.DynamicDestinationResolver;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -14,14 +17,27 @@ import javax.jms.*;
 @EnableJms
 public class AnnotationConfig {
 
-    @Bean(name = "jmsListenerContainerFactory")
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     @Autowired
-    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory, PlatformTransactionManager transactionManager, Queue queue, Topic topic) {
-        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory, PlatformTransactionManager transactionManager, DestinationResolver destinationResolver) {
+        DefaultJmsListenerContainerFactory factory =  new DefaultJmsListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
         factory.setTransactionManager(transactionManager);
+        factory.setDestinationResolver(destinationResolver);
         factory.setSubscriptionDurable(true);
+        return factory;
+    }
 
+    @Bean(name = "jmsQueueListenerContainerFactory")
+    @Autowired
+    public DefaultJmsListenerContainerFactory jmsQueueListenerContainerFactory(DefaultJmsListenerContainerFactory factory) {
+        factory.setSubscriptionDurable(false);
+        return factory;
+    }
+
+    @Bean
+    public DestinationResolver getDestinationResolver(Topic topic, Queue queue) {
         DynamicDestinationResolver destinationResolver = new DynamicDestinationResolver() {
             @Override
             public Destination resolveDestinationName(Session session, String destinationName, boolean pubSubDomain) throws JMSException {
@@ -29,8 +45,6 @@ public class AnnotationConfig {
             }
         };
 
-        factory.setDestinationResolver(destinationResolver);
-
-        return factory;
+        return destinationResolver;
     }
 }
